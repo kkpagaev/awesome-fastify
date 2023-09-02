fd0ce7a1cf519aa844ce2ff6fa2963fe4eb87f21
kkpagaev <kkpagaev@gmail.com>
2023-09-02 07:06:57 +0300

    feat(server): roles; closes #1

--- server/src/http/v1/test/get.ts
+++ server/src/http/v1/test/get.ts
@@ -1,11 +1,11 @@
+import { Role } from "@prisma/client"
 import { z } from "zod"
 
 export default createRoute({
-  method: "GET",
-  url: "/",
   query: z.object({
     id: z.string(),
   }),
+  roles: [Role.Admin],
   handler: async (_req, rep) => {
     return rep.code(200).send({
       foo: "bar",
